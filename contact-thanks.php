f71f388b217a27b62642c6d723671d09491a0589
iowageek <matthew@matthewdiiulio.com>
2014-03-01 11:42:02 -0600

    eee
    eee

---
+++ contact-thanks.php
@@ -0,0 +1,241 @@
+<!DOCTYPE HTML>
+<!--
+    Miniport 2.5 by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    -->
+<html>
+    <head>
+        <title>Matthew DiIulio</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600,700" rel="stylesheet" />
+        <script src="js/jquery.min.js"></script>
+        <script src="js/config.js"></script>
+        <script src="js/skel.min.js"></script>
+        <noscript>
+            <link rel="stylesheet" href="css/skel-noscript.css" />
+            <link rel="stylesheet" href="css/style.css" />
+            <link rel="stylesheet" href="css/style-desktop.css" />
+        </noscript>
+        <!--[if lte IE 9]>
+        <link rel="stylesheet" href="css/ie9.css" />
+        <![endif]-->
+        <!--[if lte IE 8]><script src="js/html5shiv.js"></script>
+        <link rel="stylesheet" href="css/ie8.css" />
+        <![endif]-->
+        <!--[if lte IE 7]>
+        <link rel="stylesheet" href="css/ie7.css" />
+        <![endif]-->
+        <?php
+    </head>
+    <body>
+        <!-- Nav -->
+        <nav id="nav">
+            <ul class="container">
+                <li><a href="#top">Top</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="http://iowageek.tumblr.com/">Blog</a></li>
+            </ul>
+        </nav>
+        <!-- Home -->
+        <div class="wrapper wrapper-style1 wrapper-first">
+            <article class="container" id="top">
+                <div class="row">
+                    <div class="4u">
+                        <span class="me image image-full"><img src="images/pic1.jpg" alt="" /></span>
+                    </div>
+                    <div class="8u">
+                        <header>
+                            <h1>Hi. I'm <strong>Matthew DiIulio</strong>.</h1>
+                        </header>
+                        <p>
+                        <p>And this is <strong>Me</strong>, a musician and a technology lover. I currently reside in Western Illinois</p>
+                        <a href="#work" class="button button-big">Learn about what I do</a>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <!-- Work -->
+        <div class="wrapper wrapper-style2">
+            <article id="work">
+                <header>
+                    <h2>I write articles, love technology, program and play drums.</h2>
+                    <span>Here are some of the things I do.</span>
+                </header>
+                <div class="container">
+                    <div class="row">
+                        <div class="4u">
+                            <section class="box box-style1">
+                                <img src="images/html.png"></span>
+                                <h3>Code and program</h3>
+                                <p>Coding is my favourite hobby! I believe it is important for everyone to learn how to code, because it makes you a better user and helps you understand what you are using. The program languages I'm learning how to code are HTML, PHP, JavaScript, Java, and Ruby. </p>
+                            </section>
+                        </div>
+                        <div class="4u">
+                            <section class="box box-style1">
+                                <img src="images/article.png"></span>
+                                <h3>Write Articles</h3>
+                                <p>I currently write technology related articles for my tech blog, Rotto.co and for Alleman High School's "Pioneer Press" newspaper.</p>
+                            </section>
+                        </div>
+                        <div class="4u">
+                            <section class="box box-style1">
+                                <img src="images/drums.png"></span>
+                                <h3>Play drums</h3>
+                                <p>I love music! My favourite instrument is drums. I'm currently participating musically in West Music's Weekend Warriors and my own side project with my friend Adrian Connoly. I also participate in Rock Camp every summer..</p>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <footer>
+                    <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
+                    <a href="#portfolio" class="button button-big">See some of my recent work</a>
+                </footer>
+            </article>
+        </div>
+        <!-- Portfolio -->
+        <div class="wrapper wrapper-style3">
+            <article id="portfolio">
+                <header>
+                    <h2>Awesome work.</h2>
+                    <span>Here is some of the awesome work I do.</span>
+                </header>
+                <div class="container">
+                    <div class="row">
+                        <div class="12u">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="4u">
+                            <article class="box box-style2">
+                                <a href="http://rotto.co/?p=38" class="image image-full"><img src="images/nsa.jpg" alt="" /></a>
+                                <h3><a href="http://rotto.co/?p=38">The NSA Surveillance Program and What it Means for You.</a></h3>
+                                <p>This is one of my best articles so far. This article highlights how the NSA is spying on you and what data they have about you..</p>
+                            </article>
+                        </div>
+                        <div class="4u">
+                            <a href="http://rotto.co/" class="image image-full"><img src="images/rotto.png" alt="" /></a>								
+                            <article class="box box-style2">
+                                <h3><a href="http://rotto.co/">My tech news blog, Rotto.co</a></h3>
+                                <p>You can read all my articles here.</p>
+                            </article>
+                        </div>
+                        <div class="4u">
+                            <article class="box box-style2">
+                                <a href="https://www.youtube.com/watch?v=PNCOiLvPt5o" class="image image-full"><img src="images/rme.jpg" alt="" /></a>
+                                <h3><a href="https://www.youtube.com/watch?v=PNCOiLvPt5o">Rock Camp USA</a></h3>
+                                <p>Watch a video of my performance at River Music Experience.</p>
+                            </article>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="4u">
+                            <article class="box box-style2">
+                                <a href="https://www.youtube.com/user/SuperMattTime" class="image image-full"><img src="images/yt.png" alt="" /></a>
+                                <h3><a href="https://www.youtube.com/user/SuperMattTime">My YouTube channel.</a></h3>
+                                <p>I regularly post content to my YouTube channel</p>
+                            </article>
+                        </div>
+                        <div class="4u">
+                            <article class="box box-style2">
+                                <a href="http://www.codecademy.com/digitalrunner81699" class="image image-full"><img src="images/ca.png" alt="" /></a>
+                                <h3><a href="http://www.codecademy.com/digitalrunner81699">Code Academy</a></h3>
+                                <p>Come learn computer and web programming on Code Academy with me.</p>
+                            </article>
+                        </div>
+                        <div class="4u">
+                            <article class="box box-style2">
+                                <a href="http://flypixel.com/n33" class="image image-full"><img src="images/gp.png" alt="" /></a>
+                                <h3><a href="http://flypixel.com/n33">See what I;m up to on Google+</a></h3>
+                                <p></p>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+                <footer>
+                    <p></p>
+                    <a href="#contact" class="button button-big">Get in touch with me</a>
+                </footer>
+            </article>
+        </div>
+        <!-- Contact -->
+        <div class="wrapper wrapper-style4">
+            <article id="contact" class="container small">
+                <header>
+                    <h2>Want to hire me? Get in touch!</h2>
+                    <span>Email me at <a href="mailto:matthew@matthewdiiulio.com">matthew@matthewdiiulio.com</a>, or you can use the form below.</span>
+                </header>
+					<div>
+						<div class="row">
+							<div class="12u">
+								<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;" onload="if(submitted) {window.location='http://matthewdiiulio.com/';}"></iframe>	
+						<form action="https://docs.google.com/forms/d/19FzAXRt6iu3gHSeJTh5DdkBOPqwWMJ03Jd3zO-uHeO8/formResponse" method="post" target="hidden_iframe" onsubmit="submitted=true;">
+									<div>
+										<div class="row half">
+											<div class="6u">
+												<input type="text" name="entry.1493291652" value="" class="ss-q-short required" id="entry_1493291652" dir="auto" placeholder="Name (This is Required)" />
+											</div>
+											<div class="6u">
+												<input type="text" name="entry.858665878" value="" class="ss-q-short" id="entry_858665878" dir="auto" placeholder="Your Email (This is Required)" />
+											</div>
+										</div>
+										<div class="row half">
+											<div class="12u">
+												<input type="text" name="entry.1404177569" value="" class="ss-q-short" id="entry_1404177569" dir="auto" placeholder="Subject" />
+											</div>
+										</div>
+										<div class="row half">
+											<div class="12u">
+												<textarea name="entry.1336282006" class="ss-q-long" id="entry_1336282006" dir="auto" placeholder="Message (This is Required)"></textarea>
+											</div>
+										</div> 
+										<div class="row">
+											<div class="12u">
+												<a href="input type="submit" name="submit" value="Submit" id="ss-submit" class="button form-button-submit">Send Message</a>
+												<a href="#" class="button button-alt form-button-reset">Clear Form</a>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+                <div class="row">
+                    <div class="12u">
+                        <hr />
+                        <h3>Find me on ...</h3>
+                        <ul class="social">
+                            <li class="twitter"><a href="https://twitter.com/MatthewDiiulio" class="fa fa-twitter"><span>Twitter</span></a></li>
+                            <li class="facebook"><a href="https://www.facebook.com/matthewdiiulio" class="fa fa-facebook"><span>Facebook</span></a></li>
+                            <li class="youtube"><a href="https://www.youtube.com/user/SuperMattTime" class="fa fa-youtube"><span>YouTube</span></a></li>
+                            <li class="instagram"><a href="http://instagram.com/matthewdiiulio" class="fa fa-instagram"><span>Instagram</span></a></li>
+                            <!--li class="github"><a href="http://github.com/n33" class="fa fa-github"><span>Github</span></a></li>
+                                <!--
+                                <li class="rss"><a href="#" class="fa fa-rss"><span>RSS</span></a></li>
+                                
+                                <li class="foursquare"><a href="#" class="fa fa-foursquare"><span>Foursquare</span></a></li>
+                                <li class="skype"><a href="#" class="fa fa-skype"><span>Skype</span></a></li>
+                                <li class="soundcloud"><a href="#" class="fa fa-soundcloud"><span>Soundcloud</span></a></li>
+                                <li class="youtube"><a href="#" class="fa fa-youtube"><span>YouTube</span></a></li>
+                                <li class="blogger"><a href="#" class="fa fa-blogger"><span>Blogger</span></a></li>
+                                <li class="flickr"><a href="#" class="fa fa-flickr"><span>Flickr</span></a></li>
+                                <li class="vimeo"><a href="#" class="fa fa-vimeo"><span>Vimeo</span></a></li>
+                                -->
+                        </ul>
+                        <hr />
+                    </div>
+                </div>
+        </div>
+        <footer>
+        <ul id="copyright">
+        <li>&copy; 2013 Matthew DiIulio</li>
+        <li>Design: <a href="http://html5up.net/">HTML5 UP</a></li>
+        </ul>
+        </footer>
+        </article>
+        </div>
+    </body>
+</html>
